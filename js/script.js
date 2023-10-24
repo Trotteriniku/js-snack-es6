2d0a7903094116a74b6059e8c6efe0bc88112a45
Trotteriniku <stefanozidda3@gmail.com>
2023-10-24 15:55:54 +0200

    snack 2 students list

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+'use strict'
+
+
+// SNACK1
 // creazione lista invitati in ordine di posto
 const invitati = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
 const tablename = 'Tavolo Vip';
@@ -11,3 +15,46 @@ const segnaposto = invitati.map((invitatiEl,index) =>{
 });
 
 console.log(segnaposto);
+
+
+// SNACK2
+
+// creazione lista studenti (oggetti)
+const studenti = 
+[
+    {
+        id : 213,
+        name: 'Marco della Rovere',
+        grades: 78
+    },
+    {
+        id : 110,
+        name: 'Paola melassa',
+        grades: 96
+    },
+    {
+        id : 250,
+        name: 'Andrea mantecatura',
+        grades: 48
+    },
+    {
+        id : 145,
+        name: 'Giula baratella',
+        grades: 74
+    },
+    {
+        id : 196,
+        name: 'luigi pirandello',
+        grades: 100
+    },
+    {
+        id : 102,
+        name: 'Piero della francesca',
+        grades: 68
+    },
+    {
+        id : 120,
+        name: 'Francesca totti',
+        grades: 84
+    }
+] 
